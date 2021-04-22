c23548fcca9074b14821e3f36188f8c89ba0fb0e
anton16 <anton@eaudeweb.ro>
2021-04-22 17:52:32 +0300

    docker-entrypoint.sh created

---
+++ docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+CMD="$1"
+
+if [[ ${CMD:0:4} = "http" ]]; then
+  GIT_SRC=$1
+  CMD="flake8"
+fi
+
+if [[ ${CMD:0:3} = "git" ]]; then
+  GIT_SRC=$1
+  CMD="flake8"
+fi
+
+if [ ! -z "$GIT_SRC" ]; then
+  cd /code
+  git clone $GIT_SRC
+  if [ ! -z "$GIT_NAME" ]; then
+     if [ -z "$GIT_BRANCH" ]; then
+       GIT_BRANCH="master"
+     fi
+     cd $GIT_NAME
+     if [ ! -z "$GIT_CHANGE_ID" ]; then
+        GIT_BRANCH=PR-${GIT_CHANGE_ID}
+        git fetch origin pull/$GIT_CHANGE_ID/head:$GIT_BRANCH
+     fi
+     git checkout $GIT_BRANCH
+     cd /code
+  fi
+fi
+
+if [ "$CMD" = "flake8" ]; then
+  find -L /code $CUSTOM_FIND -name [eE]xtensions -prune -o -name skins -prune -o -name "*.py" -print | xargs -r flake8
+else
+  exec "$@"
+fi
